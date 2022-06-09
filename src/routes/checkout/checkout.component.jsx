ba5db2b28b7d6aa6d8d11d7b4745e0c6fd7c3c9c
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2022-06-09 20:53:46 +0300

    done some basic checkout-item work

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,32 +1,37 @@
 import { useContext } from "react";
+import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 import { CartContext } from "../../contexts/cart.context";
+
+import "./checkout.styles.scss";
 
 const Checkout = () => {
     const { cartItems, addItemToCart, removeItemFromCart } =
         useContext(CartContext);
 
     return (
-        <div>
-            <h1>Checkout</h1>
-            <div>
-                {cartItems.map((cartItem) => {
-                    const { name, quantity, id } = cartItem;
-                    return (
-                        <div key={id}>
-                            <h2>{name}</h2>
-                            <span>{quantity}</span>
-                            <br />
-                            <span onClick={() => addItemToCart(cartItem)}>
-                                Increment
-                            </span>
-                            <br />
-                            <span onClick={() => removeItemFromCart(cartItem)}>
-                                Decrement
-                            </span>
-                        </div>
-                    );
-                })}
+        <div className="checkout-container">
+            <div className="checkout-header">
+                <div className="header-block">
+                    <span>Product</span>
+                </div>
+                <div className="header-block">
+                    <span>Desctiption</span>
+                </div>
+                <div className="header-block">
+                    <span>Quantity</span>
+                </div>
+                <div className="header-block">
+                    <span>Price</span>
+                </div>
+                <div className="header-block">
+                    <span>Remove</span>
+                </div>
             </div>
+
+            {cartItems.map((cartItem) => {
+                return <CheckoutItem key={cartItem.id} cartItem={cartItem} />;
+            })}
+            <span className="total">Total: 0</span>
         </div>
     );
 };
