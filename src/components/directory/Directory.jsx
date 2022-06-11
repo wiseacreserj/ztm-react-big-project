a550faaf3a95cc15fcd0ab12510bbdbdda8f1783
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2022-06-11 20:30:47 +0300

    some component refactoring and fixing clashning styles

--- src/components/directory/Directory.jsx
+++ src/components/directory/Directory.jsx
@@ -1,11 +1,11 @@
 import "./directory.styles.scss";
-import CategoryItem from "../category-item/category-item.component";
+import DirectoryItem from "../directory-item/directory-item.component";
 
 const Directory = ({ categories }) => {
     return (
         <div className="categories-container">
             {categories.map((category) => {
-                return <CategoryItem key={category.id} category={category} />;
+                return <DirectoryItem key={category.id} category={category} />;
             })}
         </div>
     );
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect, Fragment } from "react";
 import { useParams } from "react-router-dom";
 
 import { CategoriesContext } from "../../contexts/categories.context";
@@ -19,12 +19,17 @@ const Category = () => {
     console.log(products);
 
     return (
-        <div className="category-container">
-            {products &&
-                products.map((product) => {
-                    return <ProductCard key={product.id} product={product} />;
-                })}
-        </div>
+        <Fragment>
+            <h2 className="category-title">{category.toUpperCase()}</h2>
+            <div className="category-container">
+                {products &&
+                    products.map((product) => {
+                        return (
+                            <ProductCard key={product.id} product={product} />
+                        );
+                    })}
+            </div>
+        </Fragment>
     );
 };
 
