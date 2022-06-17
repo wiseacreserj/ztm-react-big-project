9bf88ab50fde4bd1438794fd65df6f2001d13b72
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2022-06-18 00:48:32 +0300

    auth and category components converted into styled-components

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -5,7 +5,7 @@ import { CategoriesContext } from "../../contexts/categories.context";
 
 import ProductCard from "../../components/product-card/product-card-component";
 
-import "./category.styles.scss";
+import { CategoryContainer, Title } from "./category.styles.jsx";
 
 const Category = () => {
     const { category } = useParams();
@@ -18,15 +18,15 @@ const Category = () => {
 
     return (
         <Fragment>
-            <h2 className="category-title">{category.toUpperCase()}</h2>
-            <div className="category-container">
+            <Title>{category.toUpperCase()}</Title>
+            <CategoryContainer>
                 {products &&
                     products.map((product) => {
                         return (
                             <ProductCard key={product.id} product={product} />
                         );
                     })}
-            </div>
+            </CategoryContainer>
         </Fragment>
     );
 };
