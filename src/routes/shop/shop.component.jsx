8d6d6191025ea6c192034b5e04f2e32d0967974c
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2022-06-26 02:10:27 +0300

    little eslint fix for shop.component

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -19,6 +19,7 @@ const Shop = () => {
             dispatch(setCategoriesMap(categoryMap));
         };
         getCategoriesMap();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
