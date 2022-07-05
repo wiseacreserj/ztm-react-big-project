072cd217d24d46710941eeeeec214bf4181a7ed1
Serhii Zadorozhnii <wiseacreserj@gmail.com>
2022-07-06 02:00:16 +0300

    add redux-thunk and start refactoring categories with thunk

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,9 +2,7 @@ import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
-import { getCategoriesAndDocs } from "../../utils/firebase/firebase.utils";
-
-import { setCategories } from "../../store/categories/category.action";
+import { fetchCategoriesStartAsync } from "../../store/categories/category.action";
 
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
@@ -13,12 +11,8 @@ const Shop = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        const getCategoriesMap = async () => {
-            const categoriesArray = await getCategoriesAndDocs();
+        dispatch(fetchCategoriesStartAsync());
 
-            dispatch(setCategories(categoriesArray));
-        };
-        getCategoriesMap();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
